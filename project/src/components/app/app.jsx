38ebda4f44b7f1f12ff6807f010b9f139962d9f5
TGlubokaia <t.glubokaia@gmail.com>
2021-07-03 18:56:48 +0300

    Создание компонента отрисовки предложения и добавление валидации props

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import MainScreen from '../main/main-screen';
+import MainScreen from '../main-screen/main-screen';
+import PropTypes from 'prop-types';
 
 function App(props) {
   const {aptCount} = props;
   return <MainScreen aptCount={aptCount} />;
 }
+
+App.propTypes = {
+  aptCount: PropTypes.number.isRequired,
+};
 
 export default App;
