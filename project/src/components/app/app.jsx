e43822d6b90bab256eff50308175472c2efe00cd
TGlubokaia <t.glubokaia@gmail.com>
2021-07-10 16:54:49 +0300

    Добавление машрутизации
    Добавление машрутизации, исправление разметки, добавление директорий для страниц, добавление страницы 404

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import SignInScreen from '../sign-in-screen/sign-in-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import RoomScreen from '../room-screen/room-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 import PropTypes from 'prop-types';
+import {AppRoute} from '../../const.js';
+
 
 function App(props) {
-  const {aptCount} = props;
-  return <MainScreen aptCount={aptCount} />;
+  const { aptCount } = props;
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <MainScreen
+            aptCount={aptCount}
+          />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <FavoritesScreen />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <RoomScreen />
+        </Route>
+        <Route exact path={AppRoute.LOGIN}>
+          <SignInScreen />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 App.propTypes = {
