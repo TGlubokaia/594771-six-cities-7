d803caea53d7990f9d246fb37878b0a28068ca32
TGlubokaia <t.glubokaia@gmail.com>
2021-09-06 11:33:05 +0300

    Дополнение валидации пропсов для классов

--- project/src/common/prop-types/classes.prop.js
+++ project/src/common/prop-types/classes.prop.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 const classesProp = PropTypes.shape({
   COMMON: PropTypes.string.isRequired,
   ITEM: PropTypes.string.isRequired,
+  ADD: PropTypes.string.isRequired,
+  WRAPPER: PropTypes.string.isRequired,
 });
 
 export default classesProp;
